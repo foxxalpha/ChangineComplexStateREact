2abd8ba07c8caab8e722c442b4e1654e838fd61d
foxxalpha <64504988+foxxalpha@users.noreply.github.com>
2020-09-16 19:29:31 -0400

    CompleteAss shortened code, added arrow function.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,19 +7,18 @@ function App() {
   });
 
   function handleChange(event) {
-    const newValue = event.target.value;
-    const inputName = event.target.name;
+    const { value, name } = event.target;
 
     setFullName((prevValue) => {
-      if (inputName === "fName") {
+      if (name === "fName") {
         return {
-          firstName: newValue,
+          firstName: value,
           lastName: prevValue.lastName
         };
-      } else if (inputName === "lName") {
+      } else if (className === "lName") {
         return {
           firstName: prevValue.firstName,
-          lastName: newValue
+          lastName: value
         };
       }
     });
